c16eb76e782e82e50ae8812176e0bc9638ad8e35
Shubhamcs315 <sheteshubham315@gmail.com>
2023-08-11 15:57:18 +0530

    first commit

---
+++ api.js
@@ -0,0 +1,6 @@
+const data=[
+    {name:"gh",age:30,num:999999999},
+    {name:"shubham",age:20},
+    {name:"shubham1",age:20,y:3333}
+]
+ module.exports=data;
--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-const http=require("http");
-http.createServer((req,res)=>{
-    res.write("hi i am shubham");
-    res.end();
-}).listen(4500);
+const fs=require("fs");
+const path=require("path");
+const dir=path.join(__dirname,"files");
+for(let i=0;i<5;i++){
+    fs.writeFileSync(`${dir}/file${i}.txt`,"iam shubham shete");
+}
+fs.readdir(dir,(err,data)=>{
+    data.forEach((i)=>{
+        console.log(i);
+    })
+})
+// const fs=require("fs");
+// fs.writeFileSync("ing.txt","yyyyyyyyyyyyyyyyyrrrrrrrrrrrr")
+// console.log("juuuuuuu");
+// let d=fs.readFileSync("ing.txt")
+// let h=fs.open("ing.txt","r+",function(err,d){
+// if(err){
+//     console.log(err);
+// }
+// console.log(d.toString());
+// })
+// console.log("hiii",d);
+
+
+// const inp=process.argv;
+// console.log(inp);
+// if(inp[2]=="add"){
+//     fs.writeFileSync(inp[3],inp[4]);
+// }else if(inp[2]=="remove"){
+//     fs.unlinkSync(inp[3])
+// }else{
+//     console.log("inalid options");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http=require('http');
+// const data=require("./api");
+
+// http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-Type':'application/json'})
+//     res.write(JSON.stringify(data))
+//     res.end();
+// }).listen(5000)
